ee3462bf683500dc82f838e620006879b0bb0120
PavlinPenev <86779216+PavlinPenev@users.noreply.github.com>
2021-11-14 14:43:52 +0200

    Database Introduction

---
+++ Database Introduction/BasicSelectSomeFields.sql
@@ -0,0 +1,8 @@
+USE SoftUni
+--Post in judge the queries after line 3
+SELECT Name FROM Towns
+ORDER BY Name
+SELECT Name FROM Departments
+ORDER BY Name
+SELECT FirstName, LastName, JobTitle, Salary FROM Employees
+ORDER BY Salary DESC
---
+++ Database Introduction/IncreaseEmployeesSalary.sql
@@ -0,0 +1,5 @@
+USE SoftUni
+-- Post in judge after line 3 including
+UPDATE Employees
+SET Salary = Salary * 1.1
+SELECT Employees.Salary FROM Employees
