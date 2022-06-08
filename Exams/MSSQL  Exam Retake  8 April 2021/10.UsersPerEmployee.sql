a61e8cb06414f200b6a2d14aa24dc762d8f0e1f5
Paf <pavlinpenevv@abv.bg>
2022-06-08 20:30:04 +0300

    Deleted unnecessary files

--- Exams/MSSQL  Exam Retake  8 April 2021/10.UsersPerEmployee.sql
+++
@@ -1,6 +0,0 @@
-SELECT CONCAT(e.FirstName, ' ', e.LastName)FullName, Count(u.Id)UsersCount
-FROM Employees e
-LEFT JOIN Reports r ON r.EmployeeId = e.Id
-LEFT JOIN Users u ON u.Id = r.UserId
-GROUP BY e.FirstName, e.LastName
-ORDER BY UsersCount DESC, FullName 
